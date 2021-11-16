27f005e09805bbeff008fcbd70104d7e82ca0722
“nkusikevin” <nkusikvnhart@gmail.com>
2021-11-16 23:33:57 +0200

    adding node class

---
+++ src/Polygonlab1/Tree.java
@@ -0,0 +1,4 @@
+package Polygonlab1;
+
+public class Tree {
+}
---
+++ src/Polygonlab1/TreeNode.java
@@ -0,0 +1,12 @@
+package Polygonlab1;
+
+public class TreeNode {
+    private int data ;
+    private TreeNode leftnode;
+    private  TreeNode rightnode;
+
+    public TreeNode(int value){
+        this.data = value;
+    }
+
+}
