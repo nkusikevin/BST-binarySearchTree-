5212d1286bce78101e0efe057545a447781ad4de
“nkusikevin” <nkusikvnhart@gmail.com>
2021-11-17 22:52:26 +0200

    adding insertion and inorder tree traversal

--- src/Polygonlab1/Tree.java
+++ src/Polygonlab1/Tree.java
@@ -1,4 +1,21 @@
 package Polygonlab1;
 
 public class Tree {
+    private  TreeNode root;
+
+    public void  insert ( int value){
+        if (root == null){
+            root = new TreeNode( value);
+
+        }else {
+            root.insert(value);
+        }
+    }
+
+
+    public void inorderTr(){
+        if (root != null){
+            root.inorderTr();
+        }
+    }
 }
--- src/Polygonlab1/TreeNode.java
+++ src/Polygonlab1/TreeNode.java
@@ -5,8 +5,60 @@ public class TreeNode {
     private TreeNode leftnode;
     private  TreeNode rightnode;
 
+
+    public void insert (int value){
+        if (value == data){
+            return;
+        }
+
+        if (value< data){
+            if (leftnode == null){
+                 leftnode = new TreeNode(value);
+            }else {
+                leftnode.insert(value);
+            }
+        }else
+            if (rightnode == null){
+                rightnode = new TreeNode(value);
+            }else {
+                rightnode.insert(value);
+            }
+    }
+    public void inorderTr(){
+        if (leftnode != null){
+            leftnode.inorderTr();
+        }
+        System.out.print(data + ",");
+        if (rightnode != null){
+            rightnode.inorderTr();
+        }
+    }
+
     public TreeNode(int value){
         this.data = value;
     }
 
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeftnode() {
+        return leftnode;
+    }
+
+    public void setLeftnode(TreeNode leftnode) {
+        this.leftnode = leftnode;
+    }
+
+    public TreeNode getRightnode() {
+        return rightnode;
+    }
+
+    public void setRightnode(TreeNode rightnode) {
+        this.rightnode = rightnode;
+    }
 }
